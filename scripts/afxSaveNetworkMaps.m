84a98c4673b737b173bb2ba205dde4c0b1b146a9
Wawrzyniak <Wawrzyniak@medizin.uni-leipzig.de>
2026-04-20 21:38:40 +0200

    Create subdirectories when saving ROI maps

--- scripts/afxSaveNetworkMaps.m
+++ scripts/afxSaveNetworkMaps.m
@@ -8,6 +8,15 @@ function rois = afxSaveNetworkMaps(dat,outidx,dim,mat,rois,compress,destFolder)
     for iRoi = 1:numel(rois)
         img(outidx) = dat(:,iRoi);
         fname = fullfile(destFolder, strcat(rois(iRoi).name,'.nii'));
+
+        % in case of sub directoris in roi names
+        if contains(rois(iRoi).name, filesep)
+            [pth, ~, ~] = fileparts(fname);
+            if ~exist(pth, 'dir')
+                mkdir(pth);
+            end
+        end
+        
         afxVolumeWrite(fname,img,dim,'int16',mat,'network map',true);
         if compress
             gzip(fname);
